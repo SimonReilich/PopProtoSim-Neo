2587646f9c6f578b5b96d233dd18aab80c9b5393
SimonReilich <simon.reilich@proton.me>
2025-09-22 14:37:26 +0200

    now with random numbers, but the simulator is not yet working

--- app/Protocols/Cut.hs
+++ app/Protocols/Cut.hs
@@ -1,13 +1,13 @@
 module Protocols.Cut where
 
 import Config
-import Data.Char
 
 input :: [Int] -> Config.Configuration (Int, Int)
-input (x0 : xs) = 
+input (x0 : _) = 
     if x0 <= 0 
         then []
-        else (1, 1) : (input [(x0 - 1)])
+        else (1, 1) : input [x0 - 1]
+input [] = []
 
 delta :: (Int, Int) -> (Int, Int) -> ((Int, Int), (Int, Int))
 delta (l1, h1) (l2, h2) =
@@ -16,7 +16,7 @@ delta (l1, h1) (l2, h2) =
         else ((l1, max h1 h2), (l2, max h1 h2))
 
 output :: (Int, Int) -> Int
-output (l, h) =
+output (_, h) =
     h
     
 get :: [Int] -> Config.Input (Int, Int)
--- app/Simulator.hs
+++ app/Simulator.hs
@@ -6,37 +6,47 @@ import System.Random
 run :: Config.Input a -> Int
 run (states, delta, output) = 
     if stable (states, delta, output)
-        then output (states!!0)
+        then output (head states)
     else let
-        a1 = getRandom (size states) -1
+        a1 = selectAgent (-1) (size states)
     in let
-        a2 = getRandom (size states) a1
+        a2 = selectAgent a1 (size states)
     in let
         (q1, q2) = delta (states!!a1) (states!!a2)
     in run (repl a1 q1 (repl a2 q2 states), delta, output)
 
 stable :: Config.Input a -> Bool
 stable (states, delta, output) =
-    size (filter (\a -> output a == output (states!!0)) states) == size states
+    size (filter (\a -> output a == 3) states) == size states
 
 size :: [a] -> Int
 size [] =
     0
-size (x:xs) =
-    1 + (size xs)
+size (_:xs) =
+    1 + size xs
 
 repl :: Int -> a -> [a] -> [a]
-repl i e [] =
+repl _ _ [] =
     []
 repl i e (x:xs) =
     if i <= 0
         then e:xs
-        else x:(repl (i - 1) e xs)
+        else x:repl (i - 1) e xs
 
-getRandom :: Int -> Int -> Int
-getRandom m n =
+generator :: StdGen
+generator = mkStdGen 42
+
+selectAgent :: Int -> Int -> Int
+selectAgent i n = 
     let 
-        res = randomRIO (0, m)
-    in if res == n
-        then getRandom m n
-        else res
+        rnd :: StdGen -> (Int, StdGen)
+        rnd = uniformR (0, n - 1)
+    in let 
+        result g = 
+            let 
+                (res, gNew) = rnd g
+            in if res == i
+                then result gNew
+                else res
+    in result generator
+
