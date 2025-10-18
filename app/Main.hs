e648ad121e79823c85b4f9563d116db5428d8869
SimonReilich <simon.reilich@proton.me>
2025-10-19 00:04:26 +0200

    added cut-stat, mod-stat and cmb-stat command

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
 module Main where
 
 import Control.Monad
@@ -16,7 +17,7 @@ import System.Random
 import System.Console.Docopt
 import Text.Colour
 import Util
-import Data.Text hiding (length, head, replace, replicate, find, any, null)
+import Data.Text hiding (length, head, replace, replicate, find, any, null, map, concat, concatMap)
 import qualified Snipers as Sniper
 
 patterns :: Docopt
@@ -39,8 +40,12 @@ main = do
     delay <- getDelay args
     let proto = Protocols.Cut.get (read t)
       in case getArgWithDefault args "" (longOption "sniper") of
-        "" -> simulate proto [read x0] (if args `isPresent` longOption "manual" then Sniper.manualSniper else Sniper.noSniper) seed delay (not (args `isPresent` longOption "noCheck"))
-        rt -> simulate proto [read x0] (Snipers.randomSniper seed (read rt)) seed delay (not (args `isPresent` longOption "noCheck"))
+        "" -> do
+          result <- simulate proto [read x0] (if args `isPresent` longOption "manual" then Sniper.manualSniper else Sniper.noSniper) seed delay (not (args `isPresent` longOption "noCheck")) True
+          end result [read x0] (Protocols.Cut.test (read t))
+        rt -> do
+          result <- simulate proto [read x0] (Snipers.randomSniper seed (read rt)) seed delay (not (args `isPresent` longOption "noCheck")) True
+          end result [read x0] (Protocols.Cut.test (read t))
 
   when (args `isPresent` command "mod") $ do
     x0 <- args `getArgOrExit` argument "x0"
@@ -49,19 +54,63 @@ main = do
     delay <- getDelay args
     let proto = Protocols.Modulo.get (read m)
       in case getArgWithDefault args "" (longOption "sniper") of
-        "" -> simulate proto [read x0] (if args `isPresent` longOption "manual" then Sniper.manualSniper else Sniper.noSniper) seed delay (not (args `isPresent` longOption "noCheck"))
-        rt -> simulate proto [read x0] (Snipers.randomSniper seed (read rt)) seed delay (not (args `isPresent` longOption "noCheck"))
+        "" -> do
+          result <- simulate proto [read x0] (if args `isPresent` longOption "manual" then Sniper.manualSniper else Sniper.noSniper) seed delay (not (args `isPresent` longOption "noCheck")) True
+          end result [read x0] (Protocols.Modulo.test (read m))
+        rt -> do
+          result <- simulate proto [read x0] (Snipers.randomSniper seed (read rt)) seed delay (not (args `isPresent` longOption "noCheck")) True
+          end result [read x0] (Protocols.Modulo.test (read m))
 
   when (args `isPresent` command "cmb") $ do
-    x0 <- args `getArgOrExit` argument "x0"
-    m <- args `getArgOrExit` argument "m"
-    t <- args `getArgOrExit` argument "t"
-    seed <- getSeed args
+    x0    <- args `getArgOrExit` argument "x0"
+    m     <- args `getArgOrExit` argument "m"
+    t     <- args `getArgOrExit` argument "t"
+    seed  <- getSeed args
     delay <- getDelay args
     let proto = Protocols.Combined.get (read m) (read t)
       in case getArgWithDefault args "" (longOption "sniper") of
-        "" -> simulate proto [read x0] (if args `isPresent` longOption "manual" then Sniper.manualSniper else Sniper.noSniper) seed delay (not (args `isPresent` longOption "noCheck"))
-        rt -> simulate proto [read x0] (Snipers.randomSniper seed (read rt)) seed delay (not (args `isPresent` longOption "noCheck"))
+        "" -> do
+          result <- simulate proto [read x0] (if args `isPresent` longOption "manual" then Sniper.manualSniper else Sniper.noSniper) seed delay (not (args `isPresent` longOption "noCheck")) True
+          end result [read x0] (Protocols.Combined.test (read m) (read t))
+        rt -> do
+          result <- simulate proto [read x0] (Snipers.randomSniper seed (read rt)) seed delay (not (args `isPresent` longOption "noCheck")) True
+          end result [read x0] (Protocols.Combined.test (read m) (read t))
+
+  when (args `isPresent` command "cut-stat") $ do
+    x0Max <- args `getArgOrExit` argument "x0Max"
+    tMin  <- args `getArgOrExit` argument "tMin"
+    tMax  <- args `getArgOrExit` argument "tMax"
+    rt    <- args `getArgOrExit` longOption "sniper"
+    seed  <- getSeed args
+    mapM (\(x0, t) -> do
+      (output, _, snipes) <- simulate (Protocols.Cut.get t) [x0] (Sniper.randomSniper seed (read rt)) seed 0 True False
+      return (snipes, Protocols.Cut.test t [x0] output)
+      ) (concatMap (\x0 -> map (x0, ) [(read tMin) .. (read tMax)]) [2 .. read x0Max]) >>= print
+
+  when (args `isPresent` command "mod-stat") $ do
+    x0Max <- args `getArgOrExit` argument "x0Max"
+    mMin  <- args `getArgOrExit` argument "mMin"
+    mMax  <- args `getArgOrExit` argument "mMax"
+    rt    <- args `getArgOrExit` longOption "sniper"
+    seed  <- getSeed args
+    mapM (\(x0, m) -> do
+      (output, _, snipes) <- simulate (Protocols.Modulo.get m) [x0] (Sniper.randomSniper seed (read rt)) seed 0 True False
+      return (snipes, Protocols.Modulo.test m [x0] output)
+      ) (concatMap (\x0 -> map (x0, ) [(read mMin) .. (read mMax)]) [2 .. read x0Max]) >>= print
+
+  when (args `isPresent` command "cmb-stat") $ do
+    x0Max <- args `getArgOrExit` argument "x0Max"
+    mMin  <- args `getArgOrExit` argument "mMin"
+    mMax  <- args `getArgOrExit` argument "mMax"
+    tMin  <- args `getArgOrExit` argument "tMin"
+    tMax  <- args `getArgOrExit` argument "tMax"
+    rt    <- args `getArgOrExit` longOption "sniper"
+    seed  <- getSeed args
+    mapM (\(x0, m, t) -> do
+      (output, _, snipes) <- simulate (Protocols.Combined.get m t) [x0] (Sniper.randomSniper seed (read rt)) seed 0 True False
+      return (snipes, Protocols.Combined.test m t [x0] output)
+      ) (concatMap (\(x0, m) -> map (x0, m, ) [(read tMin) .. (read tMax)]) (concatMap (\x0 -> map (x0, ) [(read mMin) .. (read mMax)]) [2 .. read x0Max])) >>= print
+
 
 getSeed :: Arguments -> IO Int
 getSeed args =
@@ -75,34 +124,35 @@ getDelay args =
     "" -> return 0
     delay -> return (round ((read delay :: Double) * 1000000))
 
-simulate :: Eq a => Eq b => Show b => Protocol a b -> [Int] -> Sniper a s -> Int -> Int -> Bool -> IO ()
-simulate (m, d, s, o, t) x sn seed delay doCheck =
+simulate :: Eq a => Eq b => Show b => Protocol a b -> [Int] -> Sniper a s -> Int -> Int -> Bool -> Bool -> IO (b, Colour, Int)
+simulate (m, d, s, o, t) x sn seed delay doCheck doPrint =
   let helper states sniper snipes gen =
         if isStable doCheck states (m, d, s, o, t)
           then
             let Just (output, colour) = getOutput states (m, d, s, o, t)
-             in putStrLn "" >>
-             putChunksUtf8With With24BitColours [chunk (pack "Output: "), fore colour (chunk (pack (show output)))] >>
-             putStrLn ("\nMinimum required snipes: " ++ show (t x output)) >>
-             putStrLn ("Actual amount of snipes: " ++ show snipes)
+             in return (output, colour, snipes)
           else do
-            (newStates, newSniper, snipeAmount, newGen) <- step states (m, d, s, o, t) sniper gen delay doCheck
+            (newStates, newSniper, snipeAmount, newGen) <- step states (m, d, s, o, t) sniper gen delay doCheck doPrint
             helper newStates newSniper (snipes + snipeAmount) newGen
-   in putStrLn "" >> helper (Protocols.getInitial (m, d, s, o, t) x) sn 0 (mkStdGen seed)
+   in do 
+    when doPrint $ do
+      putStrLn ""
+    helper (Protocols.getInitial (m, d, s, o, t) x) sn 0 (mkStdGen seed)
 
-step :: Eq a => Eq b => Show b => Configuration a -> Protocol a b -> Sniper a s -> StdGen -> Int -> Bool -> IO (Configuration a, Sniper a s, Int, StdGen)
-step states (mapping, delta, stringify, output, test) (sniperState, sniping) gen delay doCheck = do
+step :: Eq a => Eq b => Show b => Configuration a -> Protocol a b -> Sniper a s -> StdGen -> Int -> Bool -> Bool -> IO (Configuration a, Sniper a s, Int, StdGen)
+step states (mapping, delta, stringify, output, test) (sniperState, sniping) gen delay doCheck doPrint = do
   let (a1, a2, newGen) = selectAgents states (mapping, delta, stringify, output, test) gen
    in let (q1, q2) = Protocols.deltaWrapper delta (states !! a1) (states !! a2)
        in let newStates = replace a1 q1 (replace a2 q2 states)
            in do
-            printConfig states a1 a2 stringify delay
-            printConfig newStates (-1) (-1) stringify delay
+            when doPrint $ do
+              printConfig states a1 a2 stringify delay
+              printConfig newStates (-1) (-1) stringify delay
             if not (isStable doCheck newStates (mapping, delta, stringify, output, test))
               then do
                 (newSniperState, killed) <- sniping newStates sniperState
                 case killed of
-                  Just i -> if (case newStates !! i of (b, _) -> b) 
+                  Just i -> if (case newStates !! i of (b, _) -> b)
                     then return (replace i (False, let (_, s) = newStates !! i in s) newStates, (newSniperState, sniping), 1, newGen)
                     else return (newStates, (newSniperState, sniping), 0, newGen)
                   Nothing -> return (newStates, (newSniperState, sniping), 0, newGen)
@@ -162,3 +212,10 @@ getOutput c (_, _, _, o, _) =
    in case outputs of
         [(r, colour)] -> Just (r, colour)
         _ -> Nothing
+
+end :: Show b => (b, Colour, Int) -> [Int] -> ([Int] -> b -> Int) -> IO()
+end (output, colour, snipes) x test =
+  putStrLn "" >>
+  putChunksUtf8With With24BitColours [chunk (pack "Output: "), fore colour (chunk (pack (show output)))] >>
+  putStrLn ("\nMinimum required snipes: " ++ show (test x output)) >>
+  putStrLn ("Actual amount of snipes: " ++ show snipes)
--- app/Protocols/Cut.hs
+++ app/Protocols/Cut.hs
@@ -11,11 +11,11 @@ import Util
 input :: Int -> (Int, Int)
 input _ = (1, 1)
 
-delta :: (Int, Int) -> (Int, Int) -> ((Int, Int), (Int, Int))
-delta (l1, h1) (l2, h2) =
+delta :: Int -> (Int, Int) -> (Int, Int) -> ((Int, Int), (Int, Int))
+delta t (l1, h1) (l2, h2) =
   if l1 == l2
-    then ((l1, max (l1 + 1) (max h1 h2)), (l1 + 1, max (l1 + 1) (max h1 h2)))
-    else ((l1, max h1 h2), (l2, max h1 h2))
+    then ((l1, min t (max (l1 + 1) (max h1 h2))), (l1 + 1, min t (max (l1 + 1) (max h1 h2))))
+    else ((l1, min t (max h1 h2)), (l2, min t (max h1 h2)))
 
 stringify :: Int -> (Int, Int) -> Chunk
 stringify t (l, h) =
@@ -24,16 +24,16 @@ stringify t (l, h) =
 
 output :: Int -> (Int, Int) -> (Int, Colour)
 output t (_, h) =
-  let (r, g, b) = Util.hsl2Rgb (fromIntegral h / fromIntegral t) 1.0 0.5
-  in (h, colourRGB r g b)
+  let (r, g, b) = Util.hsl2Rgb (fromIntegral (min h t) / fromIntegral t) 1.0 0.5
+  in (min h t, colourRGB r g b)
 
 test :: Int -> [Int] -> Int -> Int
 test t [x0] result =
-  if min t x0 == result 
+  if min t x0 == result
     then 0
     else 1 + test t [x0 - 1] result
 test _ _ _ =
   0
 
 get :: Int -> Protocols.Protocol (Int, Int) Int
-get t = (input, delta, stringify t, output t, test t)
+get t = (input, delta t, stringify t, output t, test t)
