4559213c81880e47f56dac2681a77e01e28288ae
SimonReilich <simon.reilich@proton.me>
2025-11-21 11:07:41 +0100

    flake should now be fixed (?)

--- flake.nix
+++ flake.nix
@@ -4,12 +4,10 @@
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     haskell-flake = {
       url = "github:srid/haskell-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -21,19 +19,6 @@
       ];
       perSystem = { self', system, lib, config, pkgs, ... }: {
         haskellProjects.default = {
-          basePackages = pkgs.haskellPackages;
-
-          # Packages to add on top of `basePackages`, e.g. from Hackage
-          packages = {
-            random.source = "1.3.1";
-            list-duplicate.source = "0.1.0.0";
-            containers.source = "0.8";
-            split.source = "0.2.5";
-            docopt.source = "0.7.0.8";
-            text.source = "2.1.3";
-            safe-coloured-text.source = "0.3.0.2";
-          };
-
           autoWire = [ "packages" "apps" "checks" ];
         };
       };
