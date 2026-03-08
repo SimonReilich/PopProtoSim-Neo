f6a3f8d6013408738e41168e99c96ac5bb466c94
SimonReilich <simon.reilich@proton.me>
2026-03-08 11:36:10 +0100

    cant check cross-compilation, removing support for systems other than x86_64-linux

--- flake.nix
+++ flake.nix
@@ -8,9 +8,6 @@
     let
       systems = [
         "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
       ];
       devSystem = "x86_64-linux";
       forAllSystems =
